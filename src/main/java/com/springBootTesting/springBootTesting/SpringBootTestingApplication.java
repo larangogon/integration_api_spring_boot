43013cfd14aedee3be51ce08824938f4063fbfa4
Leidy Arango <leidy.arango@evertecinc.com>
2023-01-01 22:13:40 -0500

    feat: add testing

--- src/main/java/com/springBootTesting/springBootTesting/SpringBootTestingApplication.java
+++ src/main/java/com/springBootTesting/springBootTesting/SpringBootTestingApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootTestingApplication {
 
 	public static void main(String[] args) {
-		//dd === sou
-		System.out.println();
 
 		SpringApplication.run(SpringBootTestingApplication.class, args);
 	}
